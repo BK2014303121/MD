e9666ec18b152e71944421a42c76fcdefa98d733
maheihei <742753771@qq.com>
2019-08-07 15:56:41 +0800

    来解决解决

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!+1");
+        System.out.println("HelloWorld!+1+来吧冲突就冲突");
 		System.out.println("HelloWorld!+1+2");
     }
 }
